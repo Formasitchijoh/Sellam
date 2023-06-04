e813feefe45178132d0f01655be7f1e0efecd224
Formasitchijoh <formasitf@gmail.com>
2023-06-04 02:35:13 +0100

    new feature , product carousel and the search input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ProductPage from './Component/ProductPage';
+import ImageSlider from './Component/imageSlider';
+import images from './resource/images';
+import SelectDropDown from './Component/SelectDropDown';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <ProductPage/>
+   </div>
   );
 }
 
