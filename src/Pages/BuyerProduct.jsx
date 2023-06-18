4f5d63df4b5b6d2574dcd56929c1f3a56864e9e2
Formasitchijoh <formasitf@gmail.com>
2023-06-18 22:06:59 +0100

    Added noela work

--- src/Pages/BuyerProduct.jsx
+++ src/Pages/BuyerProduct.jsx
@@ -18,16 +18,16 @@ export default function BuyerProduct() {
           <img src={carrage} alt="picture " class=" h-10 w-10" />
           <span class=" uppercase font-bold text-lg ml-1">champions shops</span>
         </p>
-        <div class="bg-white rounded-3xl px-4 w-full">{img}</div>
+        <div class="bg-white rounded-3xl px-4 w-full">{product.img}</div>
         <div class="flex flex-row mt-4 bg-gray-200 justify-between">
-          <p class="uppercase font-semibold text-lg">{pname}</p>
+          <p class="uppercase font-semibold text-lg">{product.pname}</p>
           <p class=" my-6 mx-18p text-lg">
             price: <span class="font-semibold ">CFA </span>
-            <span class="font-semibold text-red-700 line-through">{price}</span>
+            <span class="font-semibold text-red-700 line-through">{product.price}</span>
           </p>
           <p class=" my-6 mx-18p text-lg">
             <span class="font-semibold ">CFA </span>
-            <span class="font-semibold text-green-700">{discountPrice} </span>
+            <span class="font-semibold text-green-700">{product.discountPrice} </span>
           </p>
           <button class=" bg-green-800 text-white font-semibold rounded-lg p-4 my-6">Contact Vendor</button>
           <button class=" bg-green-800 text-white font-semibold rounded-lg p-4 my-6">Other Products</button>
