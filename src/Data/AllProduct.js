1e5f8ea936a86ba8430a99d8056e862699abebc5
Formasitchijoh <formasitf@gmail.com>
2023-06-19 03:37:38 +0100

    product data

---
+++ src/Data/AllProduct.js
@@ -0,0 +1,348 @@
+const AllProductArray = [
+    {
+        Category:'Electronics',
+        products:[
+            {
+                name:'Blender',
+                id:1,
+                price:1000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            },
+            {
+                name:'Laptop',
+                id:2,
+                price:50000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            }, {
+                name:'Electric coil',
+                id:3,
+                price:5000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            }
+        ]
+       
+    },
+    {
+        Category:'Clothings',
+        id:1,
+        products:[
+            {
+                name:'Senator',
+                price:100000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            },
+            {
+                name:'Suit',
+                id:2,
+                price:50000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            }, {
+                name:'Agbada',
+                id:3,
+                price:40000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            }
+        ]
+       
+    },
+    {
+        Category:'Electronics',
+        products:[
+            {
+                name:'Tomatoes',
+                id:1,
+                price:1000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"ManiAgi shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"God Live",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            },
+            {
+                name:'Onion',
+                id:2,
+                price:50000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            }, {
+                name:'Carrot',
+                id:3,
+                price:5000,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            },{
+                name:'Green peper',
+                price:5000,
+                id:4,
+                discountPrice:0,
+                image:'',
+                shops:[
+                    {
+                        name:"Omega shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Live Score",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Bazzar shop",
+                        market:"OIC"
+                    },
+                    {
+                        name:"Young Generation",
+                        market:"Bitingi"
+                    },
+                    {
+                        name:"Spend Mony",
+                        market:"Muea"
+                    },
+                    {
+                        name:"Momo me",
+                        market:"Bitingi"
+                    },
+                    ]
+            }
+        ]
+       
+    }
+]
+
+export default AllProductArray
--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -4,11 +4,6 @@ import  ReactDOM from 'react-dom';
 import { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage,useField, useFormik} from "formik";
 import * as Yup from 'yup'
-// import '../Components/styleForm.css'
-// import eye from '../resource/'
-// import eye from '../resources/eye.png'
-// import hideeye from '../resources/eye-1.png'
-// import cap from '../resources/banana.jpg'
 import  eye from '../resource/eye.png'
 import hideeye from '../resource/eye-1.png'
 import cap from '../resource/banana.jpg'
@@ -87,7 +82,7 @@ const SignUpPage = () => {
     //defined states
     const [isShow, setisShow] = useState(false);
      const [initialValues, setinitialValues]  = useState({
-        firstName:"",
+        name:"",
         contact:"",
         email:"" ,
          password:'',
@@ -110,7 +105,7 @@ const onSubmit = (values) => {
 }
 const validationSchema =
     Yup.object({
-        firstName:Yup.string() .max(15,"Must be 15 characters or less").required("Required"),
+        name:Yup.string() .max(15,"Must be 15 characters or less").required("Required"),
         contact:Yup.string().max(20,"Must be 20 words or less") .required('Required'),
         email:Yup.string().email("Inavlid email address").required("required"),
         password:Yup.string()
@@ -137,8 +132,8 @@ const validationSchema =
         <div className=' w-full h-full mr-5 ml-5 '> 
         <Formik className="formik" initialValues={initialValues}  onSubmit={onSubmit} validationSchema={validationSchema}>
         {(formik) => (
-         <Form className='w-1/2 h-1/4 mx-auto py-5'>
-      <MyTextInput label=" Name" name="firstName" type="text" />
+         <Form className='w-full h-1/4 mx-auto py-5'>
+      <MyTextInput label=" Name" name="name" type="text" />
       <MyTextInput label="Contact" name="contact" type="text"  />    
       <MyTextInput label="Email Address" name="email" type="email" /> 
       <MyPasswordInput label='PassWord' name='password' type={isvisible} className="password" isvisible={isvisible} handleHidePassword={handleHidePassword} handleShowPassword={handleShowPassword}/>
@@ -154,29 +149,26 @@ const validationSchema =
         </div>
       <button type="submit" className='w-1/4 h-10 rounded text-2xl font-bold flex justify-center items-center mx-auto  mt-8 bg-green-950  text-white' >Login</button>
       
-      {isShow ? ( <div className='main-modal'>
+      {isShow ? ( <div className='bg-teal-500 absolute xl:bottom-1 bottom-1/4 w-full p-5 left-1/4 h-1/4  opacity-90 '>
              <div>
-                <p>FirstName: {initialValues.firstName}</p>
-                <p>LastName: {initialValues.lastName}</p>
+                <p>Name: {initialValues.name}</p>
+                <p>Contact: {initialValues.contact}</p>
+
                 <p>Email: {initialValues.email}</p>
-                <p>PassWord: {initialValues.origin}</p>
-                <p>Origin: {initialValues.inviteCode}</p>
-                <p>inviteCode: {initialValues.password}</p>
+                <p>PassWord: {initialValues.password}</p>
+                <p>Cpassword: {initialValues.confirmpassword}</p>
 
                 </div>
-            <div className='modal'>
+            <div className='w-full flex  mt-5 mb-5 justify-around items-center'>
 
-                <Link to='/LoginPage'>
-                <button type="reset" className='confirm' onClick={()=>
-                {
-                    formik.resetForm();
+                <Link to='/LoginPage'  className='w-1/4 h-1/4 text-white mb-5 rounded bg-green-950'>
+                <button type="reset" className='block items-center justify-center ' onClick={()=>
+                {formik.resetForm();
                     setisShow(false)
-                }
-                } >Confirm</button>
-
+                }} >Confirm</button>
                 </Link>
                 
-                <button className='Edit' onClick={()=> {
+                <button className='w-1/4 h-1/4  text-white mb-5 rounded bg-green-950' onClick={()=> {
                     setisShow(false)
                 }}>Edit</button>
             </div>
